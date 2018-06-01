33ba7b90b92b8d256bb39524b0b7778414aaae30
Adam Jimerson <vendion@gmail.com>
2018-06-01 13:53:11 -0400

    Only display base filename instead of full path
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- cmd/fuck.go
+++ cmd/fuck.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/palash25/foaas-cli/fucks"
 	"github.com/spf13/cobra"
@@ -30,7 +31,7 @@ var fuckCmd = &cobra.Command{
 	Short: "Request various fucks from FOaaS",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
-			fmt.Printf("you need to specifiy what type of fuck to give, see \"%s operations\" for full list\n", os.Args[0])
+			fmt.Printf("you need to specifiy what type of fuck to give, see \"%s operations\" for full list\n", path.Base(os.Args[0]))
 			os.Exit(1)
 		}
 		switch args[0] {
