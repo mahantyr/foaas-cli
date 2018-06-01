3bbf129f60280df1335281d99f3fecb37d29851d
Adam Jimerson <vendion@gmail.com>
2018-06-01 13:40:22 -0400

    Format the display of operations subcommand so only fuck name is displayed
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- cmd/operations.go
+++ cmd/operations.go
@@ -1,16 +1,38 @@
 package cmd
 
 import (
-  "fmt"
-  "github.com/palash25/foaas-cli/fucks"
-  "github.com/spf13/cobra"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/palash25/foaas-cli/fucks"
+	"github.com/spf13/cobra"
 )
 
+type operation struct {
+	Name   string            `json:"name"`
+	URL    string            `json:"url"`
+	Fields []operationFields `json:"fields"`
+}
+
+type operationFields struct {
+	Name  string `json:"name"`
+	Field string `json:"field"`
+}
+
 var operationsCmd = &cobra.Command{
-  Use:   "operations",
-  Short: "Print the operations of FOaaS",
-  Run: func(cmd *cobra.Command, args []string) {
-    ops := fucks.GetOperations()
-    fmt.Println(ops)
-  },
+	Use:   "operations",
+	Short: "Print the operations of FOaaS",
+	Run: func(cmd *cobra.Command, args []string) {
+		ops := fucks.GetOperations()
+		var opsList []operation
+		if err := json.Unmarshal([]byte(ops), &opsList); err != nil {
+			fmt.Println("failed to parse list of fucks")
+			os.Exit(1)
+		}
+
+		for i := range opsList {
+			fmt.Println(opsList[i].Name)
+		}
+	},
 }
